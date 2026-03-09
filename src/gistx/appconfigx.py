609e1b49bd812bd817a1b63394dc747de0b93c07
ykominami <ykominami@gmail.com>
2026-03-09 14:45:53 +0900

    fix

--- src/gistx/appconfigx.py
+++ src/gistx/appconfigx.py
@@ -1,7 +1,12 @@
 from yklibpy.config.appconfig import AppConfig
 
+
 class AppConfigx(AppConfig):
+    BASE_NAME_FETCH = AppConfig.BASE_NAME_FETCH
     BASE_NAME_LIST = "list"
+    BASE_NAME_GIST = "gist"
+    BASE_NAME_GISTLIST_TOP = "gistlist"
+    BASE_NAME_PROGRESS = "progress"
     BASE_NAME_REPO = "repo"
 
     AppConfig.file_assoc[AppConfig.KIND_DB][BASE_NAME_LIST] = {
@@ -10,9 +15,13 @@ class AppConfigx(AppConfig):
         AppConfig.PATH: {},
         AppConfig.VALUE: {},
     }
+    AppConfig.directory_assoc[AppConfig.KIND_DB][BASE_NAME_GISTLIST_TOP] = {
+        AppConfig.PATH: {},
+    }
     AppConfig.directory_assoc[AppConfig.KIND_DB][BASE_NAME_REPO] = {
         AppConfig.PATH: {},
     }
+
     KEY_USER = "user"
     KEY_URL_API = "url_api"
     KEY_GISTS = "gists"
